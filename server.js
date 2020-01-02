894a0e7639cecc7750bd7d3443cc87df0a42e625
elijah688 <elijah.iliev.95@gmail.com>
2020-01-02 04:27:23 +0000

    refactor(reuserly.backend.server): added port normalization

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const http = require('http');
-const app = require('./app')
+const app = require('./app.js');
 
-const port = (3000 || process.env.PORT)
+const normalizePort = val => {
+    var port = parseInt(val, 10);
+  
+        if (isNaN(port)) {
+          // named pipe
+            return val;
+      }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  };
+
+const port = normalizePort(process.env.PORT || "3000");
+
 const server = http.createServer(app);
-
-
 server.listen(port, () => {
-    console.log(`Server started on port: ${port}`);
+  console.log(`Server running at http://${port}/`);
 });
