7aa03aef66b9ced0a2f717676fc2fe6203043326
elijah688 <elijah.iliev.95@gmail.com>
2020-01-02 04:36:20 +0000

    refactor(reuserly-app-backend.server): added port normalization

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = require('./app.js');
 const normalizePort = val => {
     var port = parseInt(val, 10);
   
-        if (isNaN(port)) {
-          // named pipe
-            return val;
-      }
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
   
     if (port >= 0) {
       // port number
